5cf606c6e963cbd6936a5b20662d3881e9657aaa
Rafal Nitychoruk <info@aparatowo.pl>
2021-10-12 16:47:18 +0200

    initial init

---
+++ src/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from database.db import get_session
+from database.models import Image
+
+app = Flask(__name__)
+
+
+def db_query(query):
+    def perform_query(*args, **kwargs):
+        session = get_session()
+        result = query(session, *args, **kwargs)
+        session.close()
+        return result
+
+    return perform_query
+
+
+@db_query
+def get_count(session):
+    return session.query(Image.id).count()
+
+
+@db_query
+def get_link(session, requested_id):
+    return session.query(Image.id == requested_id).one()
+
+
+@app.route('/', methods=["GET"])
+def main_view():  # put application's code here
+
+    id = request.get["id"]
+    link = get_link(id)
+
+    conn = get_session()
+    conn.add(Image(link=link))
+
+    return
+
+
+@app.route('count/', methods=["GET"])
+def count_view():
+    return get_count()
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ src/database/db.py
@@ -0,0 +1,37 @@
+import os
+from database.models import Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import event, create_engine
+
+
+ROOT_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), ".")
+)
+
+
+def _fk_pragma_on_connect(dbapi_con, con_record):
+    """
+    Turns on foreign keys while connecting to database. Used in SQLAlchemy event listener.
+    :param dbapi_con:
+    :return:
+    """
+    dbapi_con.execute('pragma foreign_keys=ON')
+
+
+def get_session(echo=False):
+    """
+    Establishes and maintain connection with production PosgreSQL database.
+    :param echo:
+    :return:
+    """
+    db_user = os.environ["POSTGRES_USER"]
+    db_pass = os.environ["POSTGRES_PASSWORD"]
+    db_name = os.environ["POSTGRES_DB"]
+    db_address = os.environ["DB_PATH"]
+    db_path = f"postgresql+psycopg2://{db_user}:{db_pass}@{db_address}/{db_name}"
+    engine = create_engine(db_path, echo=echo)
+    db_session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    event.listen(engine, "connect", _fk_pragma_on_connect)
+
+    return db_session()
