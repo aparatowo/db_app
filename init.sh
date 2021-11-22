f93f1639422ac432364de9e45bdd2d6f558a274a
Rafal Nitychoruk <info@aparatowo.pl>
2021-11-22 11:59:36 +0100

    simple script

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum update -y
+aws s3 cp s3://git-like-bucket/db_app-master.zip ./
+unzip db_app-master.zip
+cd db_app-master
+pip3 install -r requirements.txt
+cd src
+python3 -m app
--- src/app.py
+++ src/app.py
@@ -40,4 +40,4 @@ def count_view():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
