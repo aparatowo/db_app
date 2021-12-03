f2e2ce47711983e5b907161a8310218da9c3d40f
Rafal Nitychoruk <info@aparatowo.pl>
2021-12-03 14:20:19 +0100

    correct s3 address

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
-aws s3 cp s3://git-like-bucket/db_app-master.zip ./
+aws s3 cp s3://rnitychoruk-git-like-bucket/db_app-master.zip ./
 unzip db_app-master.zip
 cd db_app-master
 pip3 install -r requirements.txt
