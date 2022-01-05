daf68f1f7c3304193cbc4b734d261458791b6c09
Rafal Nitychoruk <info@aparatowo.pl>
2022-01-05 10:40:31 +0100

    correct db credentials access, add service

--- init.sh
+++ init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 yum update -y && su ec2-user && aws s3 cp s3://rnitychoruk-git-like-bucket/db_app-master.zip ./ && unzip db_app-master.zip
 cd db_app-master && pip3 install -r requirements.txt && cd src
-touch /etc/systemd/system/db_app.service
-echo "[Unit]
+echo '[Unit]
 Description=db_app service
 
 [Service]
-ExecStart=./db_app-master/src/ && python3 -m app
+WorkingDirectory=./db_app-master/src/ && python3 -m app
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/db_app.service
-sudo systemctl start db_app && sudo systemctl enable db_app
+WantedBy=multi-user.target' > /etc/systemd/system/db_app.service
+systemctl enable db_app && systemctl start db_app
