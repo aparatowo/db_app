a800d13d6c64eb42972aebfa65c9eebd4ce23aad
Rafal Nitychoruk <info@aparatowo.pl>
2022-01-19 14:09:12 +0100

    correct db credentials access, add service

--- src/database/credentials.py
+++ src/database/credentials.py
@@ -10,7 +10,7 @@ from botocore.exceptions import ClientError
 
 def get_secret():
 
-    secret_name = "rnitychoruk-new-secret"
+    secret_name = "arn:aws:secretsmanager:eu-north-1:890769921003:secret:rnitychoruk-database_1-BRFzsa"
     region_name = "eu-north-1"
 
     # Create a Secrets Manager client
